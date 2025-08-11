8b9f295da946aa2319e2402f5de52b3e49c3b044
hguarido55 <hguarido@gmail.com>
2025-08-11 20:37:30 +0200

    Lógica de controlador para buscar usuarios recomendados

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,5 +1,22 @@
-export async function getRecommendedUsers (req, res) {
+import User from "../models/User.js";
 
+export async function getRecommendedUsers (req, res) {
+    try {
+        const currentUserId = req.user.id;
+        const currentUser = req.user; // También sirve: await User.findById(currentUserId)
+
+        const recommendedUsers = await User.find({ // Query en MongoDB para devolver usuarios recomendados
+            $and: [ // Todas las condiciones tienen que cumplirse
+                {_id: {$ne: currentUserId}}, // Descartamos el propio usuario
+                {$id: {$nin: currentUser.friends}}, // Descartamos los amigos del usuario
+                {isOnboarded: true} // Únicamente usuarios que estan oboarded (han completado la página de información adicional)
+            ]
+        });
+        res.status(200).json({ success: true, recommendedUsers});
+    } catch (error) {
+        console.error("Error al cargar usuarios recomendados", error.message);
+        res.status(500).json({ success: false, message: "Error en la recomendación de usuarios" });
+    }
 };
 
 export async function getMyFriends (req, res) {
