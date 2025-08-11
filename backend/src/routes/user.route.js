fbaa5bb09b884cf0551c4c1bebe74ea4ef5b4685
hguarido55 <hguarido@gmail.com>
2025-08-11 21:12:05 +0200

    Corregir error último commit

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -10,6 +10,6 @@ router.use(protectRoute);
 router.get("/", getRecommendedUsers);
 router.get("/friends", getMyFriends);
 
-
+router.post("/friend-request/:id", sendFriendRequest);
 
 export default router;
