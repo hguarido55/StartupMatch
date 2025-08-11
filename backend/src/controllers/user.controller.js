8cf8cb69e35125dc02c0b7fd01781e658da2dd92
hguarido55 <hguarido@gmail.com>
2025-08-11 18:28:10 +0200

    Crear archivo controlador para rutas de usuario y declarar funciones iniciales

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,7 @@
+export async function getRecommendedUsers (req, res) {
+
+};
+
+export async function getMyFriends (req, res) {
+
+};
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { getMyFriends, getRecommendedUsers } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
