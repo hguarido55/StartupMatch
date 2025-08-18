f503c171bc8729fa05d47d9b807bfd5765dfdb7c
hguarido55 <hguarido@gmail.com>
2025-08-18 22:40:45 +0200

    Configuración TanStack Query React y Axios

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,15 +5,20 @@ import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.routes.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 // Cargar variables de entorno del archivo .env
 dotenv.config();
 
+const app = express();
+
 // Puerto
 const PORT=process.env.PORT;
 
-const app = express();
-
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,8 +8,21 @@ import NotificationsPage from "./pages/NotificationsPage.jsx";
 import CallPage from "./pages/CallPage.jsx";
 import ChatPage from "./pages/ChatPage.jsx";
 import { Toaster } from 'react-hot-toast';
+import { useQuery } from '@tanstack/react-query';
+import { axiosInstance } from './lib/axios.js';
 
 const App = () => {
+  const {data} = useQuery({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/auth/me");
+      return res.data;
+    },
+    retry: false,
+  }); 
+
+  console.log(data);
+
   return (
     <div className="h-screen" data-theme="night">
       <Routes>
