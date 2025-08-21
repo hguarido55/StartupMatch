644c437182867a93ce1a54218bd5b1db44a4ddf7
hguarido55 <hguarido@gmail.com>
2025-08-21 21:05:42 +0200

    Componente React de página cargando y modularización de código (hook para autenticar)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,24 +8,15 @@ import NotificationsPage from "./pages/NotificationsPage.jsx";
 import CallPage from "./pages/CallPage.jsx";
 import ChatPage from "./pages/ChatPage.jsx";
 import { Toaster } from 'react-hot-toast';
-import { useQuery } from '@tanstack/react-query';
-import { axiosInstance } from './lib/axios.js';
+import PageLoader from './components/PageLoader.jsx';
+import useAuthUser from './hooks/useAuthUser.js';
 
 const App = () => {
-  const {
-    data: authData,
-    isLoading,
-    error,
-    } = useQuery({
-    queryKey: ["authUser"],
-    queryFn: async () => {
-      const res = await axiosInstance.get("/auth/me");
-      return res.data;
-    },
-    retry: false,
-  }); 
+  const {isLoading, authUser} = useAuthUser();
 
-  const authUser = authData?.user;
+  if(isLoading) {
+    return <PageLoader />;
+  }
 
   return (
     <div className="h-screen" data-theme="night">
