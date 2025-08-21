bb5445d4e0a670267531213a4a6144cac95c831f
hguarido55 <hguarido@gmail.com>
2025-08-21 22:46:12 +0200

    Avances en la página de onboarding - Frontend

--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -1,8 +1,145 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useAuthUser from '../hooks/useAuthUser';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { completeOnboarding } from '../lib/api';
+import toast from 'react-hot-toast';
+import { CameraIcon, ShuffleIcon } from 'lucide-react';
+import { LANGUAGES } from '../constants';
 
 const OnboardingPage = () => {
+  const { authUser } = useAuthUser();
+  const queryClient = useQueryClient();
+
+  const [formState, setFormState] = useState({
+    fullName: authUser?.fullName || "",
+    bio: authUser?.bio || "",
+    nativeLanguage: authUser?.nativeLanguage || "",
+    learningLanguage: authUser?.learningLanguage || "",
+    location: authUser?.location || "",
+    profilePic: authUser?.profilePic || "",
+  });
+
+  const {mutate: onboardingMutation, isPending} = useMutation({
+    mutationFn: completeOnboarding,
+    onSuccess: () => {
+      toast.success("Profile onboarded successfully");
+      queryClient.invalidateQueries({ queryKey: ["authUser"] });
+    }
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onboardingMutation(formState);
+  }
+
+  const handleRandomAvatar = () => {};
+
   return (
-    <div>OnboardingPage</div>
+    <div className="min-h-screen bg-base-100 flex items-center justify-center p-4">
+      <div className="card bg-base-200 w-full max-w-3xl shadow-xl">
+        <div className="card-body p-6 sm:p-8">
+          <h1 className="text-2xl sm:text-3xl font-bold text-center mb-6">Complete your Profile</h1>
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div className="flex flex-col items-center justify-center space-y-4">
+              <div className="size-32 rounded-full bg-base-300 overflow-hidden">
+                {formState.profilePic ? (
+                  <img
+                    src={formState.profilePic}
+                    alt="Profile Preview"
+                    className="w-full h-full object-cover"
+                  />
+                ) : (
+                  <div className="flex items-center justify-center h-full">
+                    <CameraIcon className="size-12 text-base-content opacity-40" />
+                  </div>
+                )}
+              </div>
+
+              <div className="flex items-center gap-2">
+                <button type="button" onClick={handleRandomAvatar} className="btn btn-accent">
+                  <ShuffleIcon className="size-4 mr-2" />
+                  Generate Random Avatar
+                </button>
+              </div>
+            </div>
+
+            <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Full Name</span>
+                </label>
+                <input 
+                  type="text"
+                  name="fullName"
+                  value={formState.fullName}
+                  onChange={(e) => setFormState({ ...formState, fullName: e.target.value })}
+                  className="input input-bordered w-full"
+                  placeholder="Your full name"
+                />
+              </div>
+
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Bio</span>
+                </label>
+                <input
+                  name="bio"
+                  value={formState.bio}
+                  onChange={(e) => setFormState({ ...formState, bio: e.target.value })}
+                  className="textarea textarea-bordered h-24"
+                  placeholder="Tell others about yourself and your goals"
+                />
+              </div>
+
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Native Language</span>
+                  </label>
+                  <select
+                    name="nativeLanguage"
+                    value={formState.nativeLanguage}
+                    onChange={(e) => setFormState({ ...formState, nativeLanguage: e.target.value })}
+                    className="select select-bordered w-full"
+                  >
+                    <option value="">Select your native languages</option>
+                    {LANGUAGES.map((lang) => (
+                      <option key={`native-${lang}`} value={lang.toLowerCase()}>
+                        {lang}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Learning Language</span>
+                  </label>
+                  <select
+                    name="learningLanguage"
+                    value={formState.learningLanguage}
+                    onChange={(e) => setFormState({ ...formState, learningLanguage: e.target.value })}
+                    className="select select-bordered w-full"
+                  >
+                    <option value="">Select language you're learning</option>
+                    {LANGUAGES.map((lang) => (
+                      <option key={`learning-${lang}`} value={lang.toLowerCase()}>
+                        {lang}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+
+              </div>
+
+              <div>
+                
+              </div>
+
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
