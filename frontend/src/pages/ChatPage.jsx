710784413fe4b0fa80b16a5cc6899923cf43b3e1
hguarido55 <hguarido@gmail.com>
2025-08-26 23:14:08 +0200

    Componente ChatLoader y frontend de página de chats - primera versión

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,8 +1,88 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import useAuthUser from '../hooks/useAuthUser';
+import { useQuery } from '@tanstack/react-query';
+import { getStreamToken } from '../lib/api';
+import { StreamChat } from 'stream-chat';
+import toast from 'react-hot-toast';
+
+import {
+  Channel,
+  ChannelHeader,
+  Chat,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from "stream-chat-react";
+import ChatLoader from '../components/ChatLoader';
+
+const STREAM_API_KEY = import.meta.env.VITE_STREAM_API_KEY;
 
 const ChatPage = () => {
+  const {id: targetUserId} = useParams();
+
+  const [chatClient, setChatClient] = useState(null);
+  const [channel, setChannel] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  const {authUser} = useAuthUser();
+
+  const {data: tokenData} = useQuery({
+    queryKey: ["streamToken"],
+    queryFn: getStreamToken,
+    enabled: !!authUser
+  });
+
+  useEffect(() => {
+    const initChat = async () => {
+      if(!tokenData?.token || !authUser) return;
+
+      try {
+        const client = StreamChat.getInstance(STREAM_API_KEY);
+        await client.connectUser({
+          id: authUser._id,
+          name: authUser.fullName,
+          image: authUser.profilePic
+        }, tokenData.token)
+
+        const channelId = [authUser._id, targetUserId].sort().join("-");
+        const currChannel = client.channel("messaging", channelId, {
+          members: [authUser._id, targetUserId],
+        });
+
+        await currChannel.watch();
+
+        setChatClient(client);
+        setChannel(currChannel);
+
+      } catch(error) {
+        console.log("Error in chat:", error);
+        toast.error("Could not initialize chat");
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    initChat();
+  }, [tokenData, authUser, targetUserId]);
+
+  if(loading || !chatClient || !channel) return <ChatLoader />;
+
   return (
-    <div>ChatPage</div>
+    <div className="h-[93vh]">
+      <Chat client={chatClient}>
+        <Channel channel={channel}>
+          <div className="w-full relative">
+            <Window>
+              <ChannelHeader />
+              <MessageList />
+              <MessageInput focus />
+            </Window>
+          </div>
+        </Channel>
+      </Chat>
+    </div>
   )
 }
 
